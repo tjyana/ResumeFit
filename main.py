14c8650767dd32185788ab2b5ea067ac4ff60d63
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-07-16 22:28:34 +0900

    sidebar!!! working!!!!

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 import streamlit as st
 from src.functions import compare_resume
 
+
+# # original code that works --------------------------------------
+
+# def main():
+#     # Title
+#     st.title("ResumeFit")
+#     st.write("Fill in the fields below to compare your resume to a job description.")
+
+#     # Input fields
+#     resume_text = st.text_input("Resume Information")
+#     jd_text = st.text_input("Job Description")
+
+#     # Submit button
+#     if st.button("Submit"):
+#         # Process the inputs
+#         process_inputs(compare_resume(resume_text, jd_text))
+
+# # original code that works --------------------------------------
+
 def main():
     # Title
-    st.title("ResumeFIt")
-    st.write("Fill in the fields below to compare your resume to a job description.")
+    st.sidebar.title("ResumeFit")
+    st.sidebar.write("Fill in the fields below to compare your resume to a job description.")
 
     # Input fields
-    resume_text = st.text_input("Resume Information")
-    jd_text = st.text_input("Job Description")
+    resume_text = st.sidebar.text_area("Resume Information", height=200)
+    jd_text = st.sidebar.text_area("Job Description", height=200)
 
     # Submit button
-    if st.button("Submit"):
+    if st.sidebar.button("Submit"):
         # Process the inputs
-        process_inputs(compare_resume(resume_text, jd_text))
+        st.session_state.resume_text = resume_text
+        st.session_state.jd_text = jd_text
+        st.header("Fit Score")
+        output = compare_resume(resume_text, jd_text)
+        process_inputs(output)
+
 
 def process_inputs(input1):
+    # Function to display the final output
     # Process the inputs here
-    st.write("Your results: ", input1)
+    st.write(" ", input1)
 
 
 if __name__ == "__main__":
--- src/functions.py
+++ src/functions.py
@@ -3,9 +3,11 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
+# # for testing locally --------------------------------------
 # load_dotenv()
 # goog_api_key = os.getenv('GOOGLE_API_KEY') # create a variable in .env file 'GOOGLE_API_KEY' and add the api key there
 
+# for testing on streamlit share -----------------------------
 goog_api_key = st.secrets['GOOGLE_API_KEY']
 
 def compare_resume(resume_text, jd_text):
@@ -15,20 +17,31 @@ def compare_resume(resume_text, jd_text):
     Resume: ```{resume_text}```
     Job Description: ```{jd_text}```
     Given the above resume and job description delineated by ```, identify the skills and qualifications from both.
-    Then, compare them to determine any skill gaps and estimate how qualified the individual is for the job.
+    Compare them to determine any skill gaps and estimate how qualified the individual is for the job.
     Give a percentage estimating how qualified the individual is for the job.
-    Output format should be as below:
+    For candidate information section, please cite any applicable certifications they have.
+    Please penalize heavily for any missing mandatory qualifications and reflect it in the percentage.
 
-    Here is the comparison of the resume and job description:
+    Output format should be as below:
 
     Estimated qualification percentage:
 
-    Skills and qualifications from resume:
+    Candidate information
+    Current location:
+    College degree (Bachelor's or above):
+    Japanese language ability:
+    English language ability:
+
+    Summary:
+    (please give a short summary of candidate persona.
+    example: junior level candidate with 2 years of experience in software engineering, proficient in Python, Java, and C++)
+
+    Top 5 Skills and qualifications from resume:
     - skill1
     - skill2
     - skill3
     ...
-    Skills and qualifications from job description:
+    5 main skills and qualifications from job description:
     - skill1
     - skill2
     - skill3
